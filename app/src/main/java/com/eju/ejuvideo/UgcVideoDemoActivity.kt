36233f4f569416f4efe99da7f237c2a993e2932d
shaochuankai <shaochuankai@163.com>
2021-03-10 09:11:25 +0800

    修改测试acitivity name

--- app/src/main/java/com/eju/ejuvideo/UgcVideoDemoActivity.kt
+++ app/src/main/java/com/eju/ejuvideo/UgcVideoDemoActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eju.video.EjuVideo
 
-class MainActivity : AppCompatActivity() {
+class UgcVideoDemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_ugc_video_demo)
 
         EjuVideo.startVideoRecord(this)
     }
